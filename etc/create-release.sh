4988c5d3cb9561bfdf6168f68988bdc708471af2
Devin T. Currie <devintcurrie@gmail.com>
2022-09-18 10:40:29 -0400

    Add dry run flag to create-release for testing

--- etc/create-release.sh
+++ etc/create-release.sh
@@ -17,30 +17,54 @@ bump() {
     
     # Set package.json version to next version and commit
     echo "Bumping version to $next_ver"
-    npm pkg set version=$next_ver
-    git add .
-    git commit -m "Bump version to $next_ver"
-    
-    
-    # Get commits
-    latest_commit=$(git rev-parse "v${latest_ver}" 2>/dev/null )
-    head_commit=$(git rev-parse HEAD)
-    
-    # Tag commits
-    if [ "$latest_commit" = "$head_commit" ]; then
-        echo "refusing to tag; $latest_ver already tagged for HEAD ($head_commit)"
+    if [ $dry != true ] ; then
+        npm pkg set version=$next_ver
+        git add .
+        git commit -m "Bump version to $next_ver"
+        
+        # Get commits
+        latest_commit=$(git rev-parse "v${latest_ver}" 2>/dev/null )
+        head_commit=$(git rev-parse HEAD)
+        
+        # Tag commits
+        if [ "$latest_commit" = "$head_commit" ]; then
+            echo "refusing to tag; $latest_ver already tagged for HEAD ($head_commit)"
+        else
+            echo "tagging v$next_ver $head_commit"
+            git tag "v$next_ver" $head_commit
+        fi
     else
-        echo "tagging v$next_ver $head_commit"
-        git tag "v$next_ver" $head_commit
+        echo "EXECUTING DRY RUN - NO CHANGES WILL BE COMMITTED"
+        
+        echo "npm pkg set version=$next_ver"
+        echo "git add ."
+        echo "git commit -m \"Bump version to $next_ver\""
+        
+        # Get commits
+        echo "latest_commit=$(git rev-parse \"v${latest_ver}\" 2>/dev/null )"
+        echo "head_commit=DRY_RUN_NO_COMMIT"
+        
+        # Tag commits
+        echo "tagging v$next_ver DRY_RUN_NO_COMMIT"
+        echo  "git tag \"v$next_ver\" DRY_RUN_NO_COMMIT"
     fi
 }
 
 usage() {
-    echo "Pass a bump type: major|minor|patch"
+    echo "Pass a bump type with the -t flag: major|minor|patch"
     exit 1
 }
 
-case $1 in
+while getopts t:d: flag
+do
+    echo "flag: $flag ${OPTARG}"
+    case "${flag}" in
+        t) type=${OPTARG};;
+        d) dry=${OPTARG};;
+    esac
+done
+
+case $type in
     major) bump 1 0 0;;
     minor) bump 0 1 0;;
     patch) bump 0 0 1;;
